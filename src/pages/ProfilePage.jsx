71486da8c4b682abfb024d49d472e24cefae2ed5
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-19 18:10:42 +0600

    file input field design done in profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import assets from "../assets/assets";
+
 const ProfilePage = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [name, setName] = useState("Martin Johnson");
+  const [bio, setBio] = useState("Hi Everyone, I'm useing QuickChat");
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Profile page</h1>
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile details</h3>
+          {/* ------ file -------- */}
+          <label
+            htmlFor="avater"
+            className="flex items-center gap-3 cursor-pointer"
+          >
+            <input
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              type="file"
+              id="avater"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={
+                selectedImg
+                  ? URL.createObjectURL(selectedImg)
+                  : assets.avatar_icon
+              }
+              alt="image"
+              className={`w-12 h-12 ${selectedImg && "rounded-full"}`}
+            />
+            upload profile image
+          </label>
+        </form>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 };
