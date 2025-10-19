6b6a8b8fa62957e99e0c6d9ead2e5e143b3e8c46
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-19 18:24:59 +0600

    profile page design done

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -8,10 +8,19 @@ const ProfilePage = () => {
   const [bio, setBio] = useState("Hi Everyone, I'm using QuickChat");
   const navigate = useNavigate();
 
+  // ------- handle submit ----------
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
       <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
-        <form className="flex flex-col gap-5 p-10 flex-1">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5 p-10 flex-1"
+        >
           <h3 className="text-lg">Profile details</h3>
           {/* ------ file -------- */}
           <label
@@ -65,6 +74,8 @@ const ProfilePage = () => {
             Save
           </button>
         </form>
+
+        {/* ---------- logo icon ------------ */}
         <img
           src={assets.logo_icon}
           alt="logo icon"
